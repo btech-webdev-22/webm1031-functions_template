21dea65b85affc5f0dcaa22e1e34ebeb82f66eeb
btechwebdev <44414569+btechwebdev@users.noreply.github.com>
2021-10-21 14:28:34 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//? Named Function
+// 1. Create two variables that each hold a number.
+// 2. Create a function that accepts to numbers, adds them together, and returns the value.
+// 3. Use variables made above as arguments to run the function.
+// 4. Store the value returned in a variable and log that variable to the console.
+
+//? Anonymous Function
+// 1. Create two variables that each hold a string.
+// 2. Create an anonymous function with a name that accepts two strings, combines them into one string, and returns the value.
+// 3. Use variables made above as arguments to run the anonymous function.
+// 4. Store the value returned in a variable and log that variable to the console.
