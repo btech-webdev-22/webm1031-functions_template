459942969e75487fd4a906a6d010de8008449748
John Memmott <jasonmemm@gmail.com>
2021-11-03 09:06:26 -0600

    specified instructions with more detail

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //? Named Function
-// 1. Create two variables that each hold a number.
-// 2. Create a function that accepts to numbers, adds them together, and returns the value.
-// 3. Use variables made above as arguments to run the function.
-// 4. Store the value returned in a variable and log that variable to the console.
+// 1. Create two variables that each hold a NUMBER.
+// 2. Create a function that accepts to numbers as function parameters, adds the two parameter variables together, and returns the value.
+// 3. Pass the variables made before as arguments for the function and run the function.
+// 4. Store the returned function value in a variable and log that variable to the console.
 
 //? Anonymous Function
-// 1. Create two variables that each hold a string.
-// 2. Create an anonymous function with a name that accepts two strings, combines them into one string, and returns the value.
-// 3. Use variables made above as arguments to run the anonymous function.
-// 4. Store the value returned in a variable and log that variable to the console.
+// 1. Create two variables that each hold a STRING.
+// 2. Create an anonymous function with a name that accepts two strings as function parameters, combines the parameter variables into one string, and returns the new value.
+// 3. Use variables made above as arguments for the anonymous function and run the anonymous function.
+// 4. Store the returned function value in a variable and log that variable to the console.
